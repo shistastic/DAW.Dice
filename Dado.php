4dc85ef3c81b32e52f6b5f705883fcbcd3c190d9
shistastic <adamjamesrowe@outlook.es>
2021-11-23 01:37:21 +0100

    first commit

---
+++ Dado.php
@@ -0,0 +1,28 @@
+<?php
+
+class Dado
+{
+    private $minNumDado = 0;
+    private $maxNumDado = 12;
+
+
+    public function setMinNumDado($param){
+        $this->minNumDado = $param;
+    }
+
+    public function setMaxNumDado($param){
+        $this->maxNumDado = $param;
+    }
+
+    public function getMinNumDado(){
+        return $this->minNumDado;
+    }
+
+    public function getMaxNumDado(){
+        return $this->maxNumDado;
+    }
+
+    echo
+
+
+}
